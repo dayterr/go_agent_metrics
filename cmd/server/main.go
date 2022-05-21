791ae203d34268251225709b29613cf4e419ae3d
Mariya Dayter <mvdayter@gmail.com>
2022-05-21 23:22:53 +0300

    testing commit

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,3 +1,41 @@
 package main
 
-func main() {}
+import (
+	"fmt"
+	"io/ioutil"
+	"net/http"
+	"strconv"
+	"strings"
+)
+// HelloWorld — обработчик запроса.
+var metrics = make(map[string][]int)
+func PostGauge(w http.ResponseWriter, r *http.Request) {
+	if r.Method == http.MethodPost {
+		_, err := ioutil.ReadAll(r.Body)
+		// обрабатываем ошибку
+		if err != nil {
+			http.Error(w, err.Error(), 500)
+			return
+		}
+		args := strings.Split(r.URL.Path, "/")
+		name := args[3]
+		metric, err := strconv.Atoi(args[4])
+		if err != nil {
+			http.Error(w, err.Error(), 500)
+			return
+		}
+		metrics[name] = append(metrics[name], metric)
+	} else {
+		fmt.Println("method GET", r.URL.Path)
+		args := strings.Split(r.URL.Path, "/")
+		name := args[3]
+		l := len(metrics[name]) - 1
+		m := strconv.Itoa(metrics[name][l])
+		w.Write([]byte(m))
+	}
+}
+
+func main() {
+	http.HandleFunc("/update/gauge/", PostGauge)
+	http.ListenAndServe(":8080", nil)
+}
