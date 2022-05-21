f138240cbc4b64082a68ec4ce1cec5609b6b49b1
Mariya Dayter <mvdayter@gmail.com>
2022-05-21 23:22:53 +0300

    testing commit

--- cmd/agent/main.go
+++ cmd/agent/main.go
@@ -1,3 +1,44 @@
 package main
 
-func main() {}
+import (
+	"bytes"
+	"fmt"
+	"net/http"
+	"runtime"
+	"time"
+)
+
+type gauge int64
+type counter int64
+
+func postGauge (v gauge, name string) {
+	fmt.Println("sending", name)
+	url := fmt.Sprintf("http://localhost:8080/update/gauge/%v/%v/", name, v)
+	req, err := http.NewRequest("POST", url, bytes.NewBuffer([]byte(string(v))))
+	req.Header.Set("Content-Type", "text/plain")
+
+	client := &http.Client{}
+	resp, err := client.Do(req)
+	if err != nil {
+		panic(err)
+	}
+	defer resp.Body.Close()
+}
+
+func main() {
+	m := &runtime.MemStats{}
+	ticker := time.NewTicker(10 * time.Second)
+	for {
+		runtime.ReadMemStats(m)
+		Alloc := gauge(m.Alloc)
+		BuckHashSys := gauge(m.BuckHashSys)
+		fmt.Println("getting alloc")
+		fmt.Println("getting bhs")
+		go func() {
+			<-ticker.C
+			postGauge(Alloc, "alloc")
+			postGauge(BuckHashSys, "buckhashsys")
+		}()
+		time.Sleep(2 * time.Second)
+	}
+}
