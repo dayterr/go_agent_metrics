22d774d0472cb7d2c170a1f512c5677c9cb90d7e
Mariya Dayter <mvdayter@gmail.com>
2022-05-23 23:39:06 +0300

    sending to 1st review

--- cmd/agent/agent_test.go
+++ cmd/agent/agent_test.go
@@ -5,6 +5,23 @@ import (
 	"testing"
 )
 
+type gauge float64
+
 func TestPostGauge(t *testing.T) {
-	assert.True(t, true)
+	tests := []struct {
+		name  string
+		v Gauge
+		nm string
+		tm string
+		want  error
+	}{
+		{name: "no error", v: Gauge(63.3), nm: "Some Metric", tm: "gauge", want: nil},
+		{name: "no error without decimal part", v: Gauge(63), nm: "Some Metric", tm: "gauge", want: nil},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			v := PostMetric(tt.v, tt.nm, tt.tm)
+			assert.Nil(t, v)
+		})
+	}
 }
--- cmd/agent/main.go
+++ cmd/agent/main.go
@@ -2,43 +2,94 @@ package main
 
 import (
 	"fmt"
+	"math/rand"
+	"os"
+	"os/signal"
+	"context"
 	"runtime"
 	"strconv"
+	"syscall"
 	"time"
 
 	"github.com/levigross/grequests"
 )
 
-type gauge float64
+type Gauge float64
 type counter int64
 
-var metrics = make(map[string]gauge)
+var metrics = make(map[string]Gauge)
+var counters = make(map[string]counter)
 
-func PostMetric (v gauge, name string, mt string) error {
-	fmt.Println("Sending", v)
-	url := fmt.Sprintf("http://localhost:8080/update/%v/%v/%v/", mt, name, v)
-	//req, err := http.NewRequest("POST", url, bytes.NewBuffer([]byte(strconv.Itoa(int(v)))))
+func PostMetric (v Gauge, name string, mt string) error {
+	url := fmt.Sprintf("http://localhost:8080/update/%v/%v/%v", mt, name, v)
 	_, err := grequests.Post(url, &grequests.RequestOptions{Data: map[string]string {name: strconv.Itoa(int(v))},
 		Headers: map[string]string{"ContentType": "text/plain"}})
 	if err != nil {
 		return err
 	}
+	return nil
+}
 
+func PostCounter (v counter, name string, mt string) error {
+	url := fmt.Sprintf("http://localhost:8080/update/%v/%v/%v", mt, name, v)
+	_, err := grequests.Post(url, &grequests.RequestOptions{Data: map[string]string {name: strconv.Itoa(int(v))},
+		Headers: map[string]string{"ContentType": "text/plain"}})
+	if err != nil {
+		return err
+	}
 	return nil
 }
 
 func main() {
+	cancelChan := make(chan os.Signal, 1)
+	signal.Notify(cancelChan, os.Interrupt, syscall.SIGTERM, syscall.SIGINT, syscall.SIGQUIT)
+	_, cancel := context.WithCancel(context.Background())
+	go func() {
+		<-cancelChan
+		cancel()
+	}()
 	m := &runtime.MemStats{}
 	ticker := time.NewTicker(10 * time.Second)
 	for {
 		runtime.ReadMemStats(m)
-		Alloc := gauge(m.Alloc)
-		BuckHashSys := gauge(m.BuckHashSys)
+		metrics["Alloc"] = Gauge(m.Alloc)
+		metrics["BuckHashSys"] = Gauge(m.BuckHashSys)
+		metrics["Frees"] = Gauge(m.Frees)
+		metrics["GCCPUFraction"] = Gauge(m.GCCPUFraction)
+		metrics["GCSys"] = Gauge(m.GCSys)
+		metrics["HeapAlloc"] = Gauge(m.HeapAlloc)
+		metrics["HeapIdle"] = Gauge(m.HeapIdle)
+		metrics["HeapInuse"] = Gauge(m.HeapInuse)
+		metrics["HeapObjects"] = Gauge(m.HeapObjects)
+		metrics["HeapReleased"] = Gauge(m.HeapReleased)
+		metrics["HeapSys"] = Gauge(m.HeapSys)
+		metrics["LastGC"] = Gauge(m.HeapAlloc)
+		metrics["Lookups"] = Gauge(m.Lookups)
+		metrics["MCacheInuse"] = Gauge(m.MCacheInuse)
+		metrics["MCacheSys"] = Gauge(m.MCacheSys)
+		metrics["MSpanInuse"] = Gauge(m.MSpanInuse)
+		metrics["MSpanSys"] = Gauge(m.MSpanSys)
+		metrics["Mallocs"] = Gauge(m.Mallocs)
+		metrics["NextGC"] = Gauge(m.NextGC)
+		metrics["NumForcedGC"] = Gauge(m.NumForcedGC)
+		metrics["NumGC"] = Gauge(m.NumGC)
+		metrics["OtherSys"] = Gauge(m.OtherSys)
+		metrics["PauseTotalNs"] = Gauge(m.PauseTotalNs)
+		metrics["StackInuse"] = Gauge(m.StackInuse)
+		metrics["StackSys"] = Gauge(m.StackSys)
+		metrics["Sys"] = Gauge(m.Sys)
+		metrics["TotalAlloc"] = Gauge(m.TotalAlloc)
+		metrics["RandomValue"] = Gauge(rand.Float64())
 		go func() {
 			<-ticker.C
-			PostMetric(Alloc, "alloc", "gauge")
-			PostMetric(BuckHashSys, "buckhashsys", "gauge")
+			for k, v := range metrics {
+				PostMetric(v, k, "gauge")
+			}
+			for k, v := range counters {
+				PostCounter(v, k, "counter")
+			}
 		}()
+		counters["Counter"] += 1
 		time.Sleep(2 * time.Second)
 	}
 }
