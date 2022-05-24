540e99eaa1f89ab12d4bdb3e947acdad2a018c10
Mariya Dayter <mvdayter@gmail.com>
2022-05-24 23:18:50 +0300

    fixing 1st

--- cmd/agent/main.go
+++ cmd/agent/main.go
@@ -45,53 +45,55 @@ func main() {
 	exit_chan := make(chan int)
 	m := &runtime.MemStats{}
 	ticker := time.NewTicker(10 * time.Second)
-	for {
-		runtime.ReadMemStats(m)
-		metrics["Alloc"] = Gauge(m.Alloc)
-		metrics["BuckHashSys"] = Gauge(m.BuckHashSys)
-		metrics["Frees"] = Gauge(m.Frees)
-		metrics["GCCPUFraction"] = Gauge(m.GCCPUFraction)
-		metrics["GCSys"] = Gauge(m.GCSys)
-		metrics["HeapAlloc"] = Gauge(m.HeapAlloc)
-		metrics["HeapIdle"] = Gauge(m.HeapIdle)
-		metrics["HeapInuse"] = Gauge(m.HeapInuse)
-		metrics["HeapObjects"] = Gauge(m.HeapObjects)
-		metrics["HeapReleased"] = Gauge(m.HeapReleased)
-		metrics["HeapSys"] = Gauge(m.HeapSys)
-		metrics["LastGC"] = Gauge(m.HeapAlloc)
-		metrics["Lookups"] = Gauge(m.Lookups)
-		metrics["MCacheInuse"] = Gauge(m.MCacheInuse)
-		metrics["MCacheSys"] = Gauge(m.MCacheSys)
-		metrics["MSpanInuse"] = Gauge(m.MSpanInuse)
-		metrics["MSpanSys"] = Gauge(m.MSpanSys)
-		metrics["Mallocs"] = Gauge(m.Mallocs)
-		metrics["NextGC"] = Gauge(m.NextGC)
-		metrics["NumForcedGC"] = Gauge(m.NumForcedGC)
-		metrics["NumGC"] = Gauge(m.NumGC)
-		metrics["OtherSys"] = Gauge(m.OtherSys)
-		metrics["PauseTotalNs"] = Gauge(m.PauseTotalNs)
-		metrics["StackInuse"] = Gauge(m.StackInuse)
-		metrics["StackSys"] = Gauge(m.StackSys)
-		metrics["Sys"] = Gauge(m.Sys)
-		metrics["TotalAlloc"] = Gauge(m.TotalAlloc)
-		metrics["RandomValue"] = Gauge(rand.Float64())
-		counters["Counter"] += 1
-		go func() {
-			<-ticker.C
-			for k, v := range metrics {
-				PostMetric(v, k, "gauge")
+	go func() {
+		for {
+			runtime.ReadMemStats(m)
+			metrics["Alloc"] = Gauge(m.Alloc)
+			metrics["BuckHashSys"] = Gauge(m.BuckHashSys)
+			metrics["Frees"] = Gauge(m.Frees)
+			metrics["GCCPUFraction"] = Gauge(m.GCCPUFraction)
+			metrics["GCSys"] = Gauge(m.GCSys)
+			metrics["HeapAlloc"] = Gauge(m.HeapAlloc)
+			metrics["HeapIdle"] = Gauge(m.HeapIdle)
+			metrics["HeapInuse"] = Gauge(m.HeapInuse)
+			metrics["HeapObjects"] = Gauge(m.HeapObjects)
+			metrics["HeapReleased"] = Gauge(m.HeapReleased)
+			metrics["HeapSys"] = Gauge(m.HeapSys)
+			metrics["LastGC"] = Gauge(m.HeapAlloc)
+			metrics["Lookups"] = Gauge(m.Lookups)
+			metrics["MCacheInuse"] = Gauge(m.MCacheInuse)
+			metrics["MCacheSys"] = Gauge(m.MCacheSys)
+			metrics["MSpanInuse"] = Gauge(m.MSpanInuse)
+			metrics["MSpanSys"] = Gauge(m.MSpanSys)
+			metrics["Mallocs"] = Gauge(m.Mallocs)
+			metrics["NextGC"] = Gauge(m.NextGC)
+			metrics["NumForcedGC"] = Gauge(m.NumForcedGC)
+			metrics["NumGC"] = Gauge(m.NumGC)
+			metrics["OtherSys"] = Gauge(m.OtherSys)
+			metrics["PauseTotalNs"] = Gauge(m.PauseTotalNs)
+			metrics["StackInuse"] = Gauge(m.StackInuse)
+			metrics["StackSys"] = Gauge(m.StackSys)
+			metrics["Sys"] = Gauge(m.Sys)
+			metrics["TotalAlloc"] = Gauge(m.TotalAlloc)
+			metrics["RandomValue"] = Gauge(rand.Float64())
+			counters["Counter"] += 1
+			go func() {
+				<-ticker.C
+				for k, v := range metrics {
+					PostMetric(v, k, "gauge")
+				}
+				for k, v := range counters {
+					PostCounter(v, k, "counter")
+				}
+			}()
+			time.Sleep(2 * time.Second)
+			s := <- signalChan
+			switch s {
+			case syscall.SIGTERM, syscall.SIGINT, syscall.SIGQUIT:
+				exit_chan <- 0
 			}
-			for k, v := range counters {
-				PostCounter(v, k, "counter")
-			}
-		}()
-		time.Sleep(2 * time.Second)
-		s := <- signalChan
-		switch s {
-		case syscall.SIGTERM, syscall.SIGINT, syscall.SIGQUIT:
-			exit_chan <- 0
 		}
-	}
+	}()
 	exitCode := <-exit_chan
 	os.Exit(exitCode)
 }
