9ce4747bba59e6b3de92592ff5a4b2ae79bdd7d0
Mariya Dayter <mvdayter@gmail.com>
2022-06-12 03:18:33 +0300

    added env

--- internal/config/config.go
+++ internal/config/config.go
@@ -11,8 +11,8 @@ import (
 
 type Config struct {
 	Address string `env:"ADDRESS" envDefault:"localhost:8080"`
-	ReportInterval time.Duration `env:"REPORT_INTERVAL" envDefault:10`
-	PollInterval time.Duration `env:"POLL_INTERVAL" envDefault:2`
+	ReportInterval time.Duration `env:"REPORT_INTERVAL" envDefault:"10"`
+	PollInterval time.Duration `env:"POLL_INTERVAL" envDefault:"2"`
 }
 
 func GetEnv() Config {
