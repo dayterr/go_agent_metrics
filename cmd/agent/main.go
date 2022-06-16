57fcbcbbe798751da25949de33264d5f04429cf2
Mariya Dayter <mvdayter@gmail.com>
2022-06-16 20:12:09 +0300

    testing flags

--- cmd/agent/main.go
+++ cmd/agent/main.go
@@ -7,16 +7,14 @@ import (
 	"time"
 
 	"github.com/dayterr/go_agent_metrics/internal/agent"
-	"github.com/dayterr/go_agent_metrics/internal/config"
 )
 
 func main() {
 	signalChan := make(chan os.Signal, 1)
 	signal.Notify(signalChan, os.Interrupt, syscall.SIGTERM, syscall.SIGINT, syscall.SIGQUIT)
 	exitChan := make(chan int)
-	conf := config.GetEnv()
-	ticker := time.NewTicker(conf.ReportInterval)
-	tickerMetrics := time.NewTicker(conf.PollInterval)
+	ticker := time.NewTicker(agent.ReportInterval)
+	tickerMetrics := time.NewTicker(agent.PollInterval)
 	var am agent.Storage
 	go func() {
 		for {
