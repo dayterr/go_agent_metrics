990f2e97190184b12be7f9543ce424cec9a09841
Mariya Dayter <mvdayter@gmail.com>
2022-06-18 02:33:10 +0300

    testing renewed flags

--- cmd/agent/main.go
+++ cmd/agent/main.go
@@ -11,18 +11,17 @@ import (
 	"github.com/dayterr/go_agent_metrics/internal/config"
 )
 
-var cfg = config.GetEnv()
-
 func main() {
-	flag.StringVar(&cfg.Address, "a", cfg.Address, "Address for the server")
-	flag.DurationVar(&cfg.ReportInterval, "r", cfg.ReportInterval, "Interval for sending the metrics to the server")
-	flag.DurationVar(&cfg.PollInterval, "p", cfg.PollInterval, "Interval for polling the metrics")
+	var Cfg = config.GetEnv()
+	flag.StringVar(&Cfg.Address, "a", Cfg.Address, "Address for the server")
+	flag.DurationVar(&Cfg.ReportInterval, "r", Cfg.ReportInterval, "Interval for sending the metrics to the server")
+	flag.DurationVar(&Cfg.PollInterval, "p", Cfg.PollInterval, "Interval for polling the metrics")
 	flag.CommandLine.Parse(os.Args[1:])
 	signalChan := make(chan os.Signal, 1)
 	signal.Notify(signalChan, os.Interrupt, syscall.SIGTERM, syscall.SIGINT, syscall.SIGQUIT)
 	exitChan := make(chan int)
-	ticker := time.NewTicker(cfg.ReportInterval)
-	tickerMetrics := time.NewTicker(cfg.PollInterval)
+	ticker := time.NewTicker(Cfg.ReportInterval)
+	tickerMetrics := time.NewTicker(Cfg.PollInterval)
 	var am agent.Storage
 	go func() {
 		for {
@@ -30,7 +29,7 @@ func main() {
 			case <-tickerMetrics.C:
 				am = agent.ReadMetrics()
 			case <-ticker.C:
-				agent.PostAll(am, cfg.Address)
+				agent.PostAll(am, Cfg.Address)
 			case s := <-signalChan:
 				switch s {
 				case syscall.SIGTERM, syscall.SIGINT, syscall.SIGQUIT:
