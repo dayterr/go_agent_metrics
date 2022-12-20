8a9ec8abea6ad1ff18a6dcf4990b9e9bc65029c3
Mariya Dayter <mvdayter@gmail.com>
2022-12-20 23:53:58 +0300

    sending to 1st review

--- internal/config/agent/config.go
+++ internal/config/agent/config.go
@@ -2,8 +2,11 @@ package agent
 
 import (
 	"flag"
+	"io/ioutil"
 	"log"
+	"os"
 	"time"
+	"encoding/json"
 
 	"github.com/caarlos0/env/v6"
 )
@@ -22,9 +25,20 @@ type ConfigAgent struct {
 	Key            string        `env:"KEY" envDefault:""`
 	Salt string
 	CryptoKey string
+	File           string        `env:"CONFIG" envDefault:""`
 }
 
 type FlagStruct struct {
+	Address        string
+	ReportInterval time.Duration
+	PollInterval   time.Duration
+	Key            string
+	Salt string
+	CryptoKey string
+	File string
+}
+
+type FileStruct struct {
 	Address        string
 	ReportInterval time.Duration
 	PollInterval   time.Duration
@@ -43,6 +57,7 @@ func GetEnvAgent() (ConfigAgent, error) {
 	flag.StringVar(&fs.Key, "k", defaultKey, "Key for encrypting")
 	flag.StringVar(&fs.Salt, "salt", "", "salt for crypto key")
 	flag.StringVar(&fs.CryptoKey, "cryptokey", "", "crypto key")
+	flag.StringVar(&fs.File, "c", "", "config file")
 	flag.Parse()
 
 	err := env.Parse(&cfg)
@@ -61,6 +76,69 @@ func GetEnvAgent() (ConfigAgent, error) {
 	if cfg.Key == defaultKey && fs.Key != defaultKey {
 		cfg.Key = fs.Key
 	}
+	if cfg.Salt == "" && fs.Salt != "" {
+		cfg.Salt = fs.Salt
+	}
+	if cfg.CryptoKey == "" && fs.CryptoKey != "" {
+		cfg.CryptoKey = fs.CryptoKey
+	}
+	if cfg.File == "" && fs.File != "" {
+		cfg.File = fs.File
+	}
+
+	var fileCfg FileStruct
+	if cfg.File != "" {
+		fileCfg, err = readConfigFile(cfg.File)
+		if err != nil {
+			log.Println("config file error")
+		}
+	}
+
+	if cfg.ReportInterval == defaultReportInterval && fileCfg.ReportInterval != 0 {
+		cfg.ReportInterval = fileCfg.ReportInterval
+	}
+	if cfg.PollInterval == defaultPollInterval && fileCfg.PollInterval != 0 {
+		cfg.PollInterval = fileCfg.PollInterval
+	}
+	if cfg.Address == defaultAddress && fileCfg.Address != "" {
+		cfg.Address = fileCfg.Address
+	}
+	if cfg.Key == defaultKey && fileCfg.Key != "" {
+		cfg.Key = fileCfg.Key
+	}
+	if cfg.Salt == "" && fileCfg.Salt != "" {
+		cfg.Salt = fileCfg.Salt
+	}
+	if cfg.CryptoKey == "" && fileCfg.CryptoKey != "" {
+		cfg.CryptoKey = fileCfg.CryptoKey
+	}
+
 	log.Println("agent config", cfg)
 	return cfg, nil
+}
+
+func readConfigFile(filepath string) (FileStruct, error) {
+	jsonFile, err := os.Open(filepath)
+
+	if err != nil {
+		return FileStruct{}, err
+	}
+
+	defer jsonFile.Close()
+
+	byteValue, err := ioutil.ReadAll(jsonFile)
+
+	if err != nil {
+		return FileStruct{}, err
+	}
+
+	var fs FileStruct
+
+	err = json.Unmarshal(byteValue, &fs)
+
+	if err != nil {
+		return FileStruct{}, err
+	}
+
+	return fs, nil
 }
